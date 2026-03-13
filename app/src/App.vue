5e14c08067a9afc6088a444926e954993b3c9f9e
YH4-ES <yifanh4@nycstudents.net>
2026-03-13 14:00:18 -0400

    asdf

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,6 +8,15 @@
 <script setup>
 import {ref} from "vue";
 const homelessData = ref([])
+async function getHomeless() {
+  try {
+    const response = await fetch(`https://data.cityofnewyork.us/api/v3/views/bmxf-3rd4/query.json`)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <style lang="css" scoped>
