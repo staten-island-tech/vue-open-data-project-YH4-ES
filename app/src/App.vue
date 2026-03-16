62576a18c49f2bf8523563918bb77277f0bcc249
YH4-ES <yifanh4@nycstudents.net>
2026-03-16 14:01:52 -0400

    dfgh

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,24 +1,49 @@
 <template>
-  <h1>the website for if you really hate the homeless</h1>
+  <h1>The homeless infoer</h1>
   <div>
+    <canvas id="acquisitions">
 
+    </canvas>
   </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
-const homelessData = ref([])
+import Chart from 'chart.js/auto'
+const homelessData = ref()
 async function getHomeless() {
   try {
-    const response = await fetch(`https://data.cityofnewyork.us/api/v3/views/bmxf-3rd4/query.json`)
+    const response = await fetch(`https://data.cityofnewyork.us/resource/bmxf-3rd4.json`)
     const data = await response.json()
     console.log(data)
+    homelessData.value = data
   } catch (error) {
     console.log(error)
   }
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: homelessData.value.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            home: homelessData.value.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
 }
+
+getHomeless()
+
+
 </script>
 
 <style lang="css" scoped>
-
+:global(html) {
+  font-family: Consolas, monaco, monospace;
+}
 </style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
+import App from '@/App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+       path: '/',
+      name: 'home',
+      component: App,
+    },
+  ],
 })
 
 export default router
