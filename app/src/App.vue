f59e5967b65fa1ac0f6939919969f9534eb4d913
YH4-ES <yifanh4@nycstudents.net>
2026-03-24 14:01:52 -0400

    sdfg

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,56 +3,38 @@
   <div id="dropdown">Dropdown
   </div>
   <div style="width: 1000px;">
-    <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+    <BarChart :dataset="homelessData"/>
   </div>
 </template>
 
-<script>
-import {onMounted, ref} from "vue";
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-const homelessData = ref()
-const dropOptions = ref({})
-async function getHomeless(xValue, yValue, dataset) {
-  try {
-    var response = await fetch(dataset)
-    var data = await response.json()
-    console.log(data)
-    homelessData.value = data
-    for (let key in homelessData.value[1]) {
-      dropOptions.value[key] = false
-    }
-    console.log(dropOptions)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-getHomeless("report_date", "fwc_avg_daily_census", `https://data.cityofnewyork.us/resource/5e9h-x6ak.json`)
-console.log(homelessData.value)
-
-export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: "fwc_avg_daily_census",
-          datasets: homelessData.value.map(row => row["fwc_avg_daily_census"])
-        },
-        chartOptions: {
-          responsive: true
-        }
-      }
+<script setup>
+import {onBeforeMount, onMounted, ref} from "vue";
+import BarChart from "./Components/BarChart.vue";
+const homelessData = ref([])
+onBeforeMount(()=>{
+  const dropOptions = ref({})
+  async function getHomeless(xValue, yValue, dataset) {
+    try {
+      const response = await fetch(dataset)
+      const data = await response.json()
+      console.log(data)
+      return data
+    } catch (error) {
+      console.log(error)
     }
   }
 
+  function setDropdown (sample, dropdownRoot) {
+    for (let key in sample) {
+      dropdownRoot[key] = false
+    }
+  }
 
+  let data = getHomeless("report_date", "fwc_avg_daily_census", `https://data.cityofnewyork.us/resource/5e9h-x6ak.json`)
+  setDropdown(data, dropOptions.value)
+  homelessData.value.push( data)
+  console.log(homelessData.value)
+})
 </script>
 
 <style lang="css" scoped>
---
+++ app/src/Components/BarChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <Bar
+    id="chart"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { onMounted } from 'vue'
+
+const props = defineProps({
+    dataset: {
+        type: Object,
+        required: true,
+    }
+})
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] }, {data: [5, 5, 5]} ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
