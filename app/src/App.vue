3863105911f620b32dd97f887cfe372e836e546e
YH4-ES <yifanh4@nycstudents.net>
2026-04-01 22:41:05 -0400

    jn

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-    <DropOption id="DropOption" v-for="(value, key) in dropOptions[`bar1`][`Y`]" v-show="value || openedOptions[`bar1`][`Y`]" :option="key" @click="clickOption(key, value, `bar1`, `Y`)"/>
   <div>
     <RouterView/>
   </div>
